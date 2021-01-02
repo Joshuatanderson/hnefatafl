36f4132eaa63dd766624e1c7fb7a1e17d1e5e92d
Joshua Anderson <joshuatanderson1@gmail.com>
2021-01-02 11:20:05 -0600

    feat: board constructed, markers created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import classnames from "classnames";
+
+import "./App.scss";
+
+const BLACK = 2;
+const WHITE = 1;
+const EMPTY = 0;
+
+const INIT_BOARD_STATE = [
+  [0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0],
+  [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2],
+  [2, 0, 0, 0, 1, 1, 1, 0, 0, 0, 2],
+  [2, 2, 0, 1, 1, 1, 1, 1, 0, 2, 2],
+  [2, 0, 0, 0, 1, 1, 1, 0, 0, 0, 2],
+  [2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
+  [0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0],
+];
+
+const BOARD_WIDTH = 11;
+const BOARD_HEIGHT = 11;
 
 function App() {
+  const boardState = INIT_BOARD_STATE;
+  const makeBoard = (BOARD_WIDTH: number, BOARD_HEIGHT: number) => {
+    const boardContents: JSX.Element[][] = [];
+    for (let i = 0; i < BOARD_HEIGHT; i++) {
+      const row: JSX.Element[] = [];
+      for (let j = 0; j < BOARD_WIDTH; j++) {
+        const markerClasses = classnames({
+          marker: true,
+          isWhite: boardState[i][j] === WHITE,
+          isBlack: boardState[i][j] === BLACK,
+        });
+        console.log(boardState[i][j] === BLACK);
+        row.push(
+          <div className="cell" id="i - j" key={j}>
+            <div className={markerClasses}></div>
+          </div>
+        );
+      }
+      boardContents.push(row);
+    }
+    return <div className="board">{boardContents}</div>;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Hnefatafl</h1>
+      {makeBoard(BOARD_WIDTH, BOARD_HEIGHT)}
     </div>
   );
 }
