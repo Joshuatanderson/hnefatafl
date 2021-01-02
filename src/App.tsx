99b8e4218c1ddf3b619d5f365bb6dc08c1661ad1
Joshua Anderson <joshuatanderson1@gmail.com>
2021-01-02 13:00:09 -0600

    feat: add piece selection

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import classnames from "classnames";
 
 import { boardAtom } from "./atoms/boardState";
+import { selectedMarkerAtom } from "./atoms/selectedMarker";
 import "./App.scss";
 import { useAtom } from "jotai";
 
@@ -12,9 +13,15 @@ const EMPTY = 0;
 const BOARD_WIDTH = 11;
 const BOARD_HEIGHT = 11;
 
+// highlight a piece by clicking on it.
+// Any piece that has an open space next to it should be highlightable
+
+// has valid move: get neighbors.  One neighbor or more is empty.
+
 function App() {
   const [boardState, updateBoardState] = useAtom(boardAtom);
-  
+  const [selectedMarker, updateSelectedMarker] = useAtom(selectedMarkerAtom);
+
   const makeBoard = (BOARD_WIDTH: number, BOARD_HEIGHT: number) => {
     const boardContents: JSX.Element[][] = [];
     for (let i = 0; i < BOARD_HEIGHT; i++) {
@@ -25,10 +32,13 @@ function App() {
           isWhite: boardState[i][j] === WHITE,
           isBlack: boardState[i][j] === BLACK,
         });
-        console.log(boardState[i][j] === BLACK);
         row.push(
-          <div className="cell" id="i - j" key={j}>
-            <div className={markerClasses}></div>
+          <div className="cell" id={`${i}-${j}-cell`} key={`${i}-${j}`}>
+            <div
+              id={`${i}-${j}-marker`}
+              className={markerClasses}
+              onClick={() => getNeighbors(i, j)}
+            ></div>
           </div>
         );
       }
@@ -36,6 +46,40 @@ function App() {
     }
     return <div className="board">{boardContents}</div>;
   };
+
+  const getNeighbors = (row: number, column: number): boolean => {
+    const neighbors = {
+      north: boardState[row - 1]?.[column],
+      south: boardState[row + 1]?.[column],
+      east: boardState[row][column + 1],
+      west: boardState[row][column - 1],
+    };
+    for (let prop in neighbors) {
+      if (neighbors[prop as "north" | "south" | "east" | "west"] === EMPTY) {
+        removeCurrentHighlight();
+        highlightMarker(row, column);
+        updateSelectedMarker(`${row}-${column}`)
+        return true;
+      }
+    }
+    console.log(false);
+    return false;
+  };
+
+  const highlightMarker = (row: number, column: number) => {
+    document
+      .getElementById(`${row}-${column}-marker`)
+      ?.classList?.add("highlight");
+  };
+
+  const removeCurrentHighlight = () => {
+    console.log(selectedMarker)
+    const el = document.getElementById(`${selectedMarker}-marker`)
+    console.log(el);
+    if(el){
+      el?.classList.remove("highlight")
+    }
+  }
 
   return (
     <div className="App">
